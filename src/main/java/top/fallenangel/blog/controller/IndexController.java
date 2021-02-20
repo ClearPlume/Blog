865c8233d4d8bce86c3379ad7e036fb9c401e4b1
FallenAngel <the.fallenangel.965@gmail.com>
2021-02-20 14:23:13 +0800

    Initial commit

---
+++ src/main/java/top/fallenangel/blog/controller/IndexController.java
@@ -0,0 +1,12 @@
+package top.fallenangel.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping({"index", "/"})
+    public String index() {
+        return "index";
+    }
+}
